d09090eba32311157f157e2326e58c4396f046cc
sarahnvo <155112757+sarahnvo@users.noreply.github.com>
2023-12-30 18:06:21 +0000

    using for loop for my options
    if, elif, and else statements

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('hello world!')
+print('Welcome to the Elite101 Chatbox!')
+
+name = input('What is your name?')
+print(f'Its nice to meet you {name}! ')
+
+age = input('How old are you?')
+print(f'{age} is a great age to be! How can I help you today?')
+
+options = ['option 1', 'option 2', 'option 3', 'Exit the conversation']
+
+for option in options:
+  
