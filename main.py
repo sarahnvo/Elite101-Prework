fc7943ec3efbda01054426e32fe8bd4c117b320a
sarahnvo <155112757+sarahnvo@users.noreply.github.com>
2024-01-06 21:07:00 +0000

    separate list and if statements
    removed if statements from the for loop

--- main.py
+++ main.py
@@ -4,9 +4,28 @@ name = input('What is your name?')
 print(f'Its nice to meet you {name}! ')
 
 age = input('How old are you?')
-print(f'{age} is a great age to be! How can I help you today?')
+print(f'{age} is a great age to be! How can I help you today?\n')
 
+
+print('Please choose from the following options:')
 options = ['option 1', 'option 2', 'option 3', 'Exit the conversation']
-
 for option in options:
-  
+  print(option)
+
+option_choice = input('Choose an option:')
+if option_choice == 'option 1':
+  print('you chose option 1')
+elif option_choice == 'option 2':
+  print('you chose option 2')
+elif option_choice == 'option 3':
+  print('you chose option 3')
+elif option_choice == 'Exit the conversation':
+  print(f'Goodbye {name}, have a great day!')
+else:
+  print('Invalid option')
+
+
+
+
+
+
